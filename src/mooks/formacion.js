f4bb94b68c50f95f974a55b736668fe03a33fafd
Daniel Ruiz <danuruiz7@hotmail.com>
2023-11-20 17:10:28 +0100

    agregando al repo

---
+++ src/mooks/formacion.js
@@ -0,0 +1,39 @@
+export const formacion = [
+  {
+    //GRUPO ATRIUM
+    sitioURL:
+      'https://www.grupoatrium.com/cursos-expertos/curso-experto-de-full-stack-developer/',
+    academiaLogo:
+      'https://campusatrium.com/web/css/themes/blue_lagoon/images/header-logo-custom1.png',
+    academia: 'Grupo Atrim',
+    grado: 'Desarrollador Web FullStack',
+    fecha: 'Febrero 2023 - Julio 2023',
+    subtitulo: 'Proyecto Final',
+    link: 'https://tasks-app-by-danuruiz7.vercel.app/',
+    nombreLink: 'Task App (CRUD)',
+  },
+  {
+    //ACADEMIA X REACT
+    sitioURL: 'https://www.academia-x.com/',
+    academiaLogo:
+      'https://kajabi-storefronts-production.kajabi-cdn.com/kajabi-storefronts-production/file-uploads/themes/2147839398/settings_images/27f5c58-62-fdb6-87f-aa61016e51b6_HMS6S10GQK6r4Q9lCruv_academia_logo_large.png',
+    academia: 'Academia X',
+    grado: 'Clase Maestra React',
+    fecha: '11 / Agosto / 2023',
+    subtitulo: 'Certificado',
+    link: 'https://docs.google.com/feeds/download/presentations/Export?id=1ixmljeNN16YadaXYeUZ2ji_inz5k_EjHPC0bbIwxJao&exportFormat=pdf',
+    nombreLink: 'Clase Maestra React',
+  },
+  {
+    //ACADEMIA X NODE
+    sitioURL: 'https://www.academia-x.com/',
+    academiaLogo:
+      'https://kajabi-storefronts-production.kajabi-cdn.com/kajabi-storefronts-production/file-uploads/themes/2147839398/settings_images/27f5c58-62-fdb6-87f-aa61016e51b6_HMS6S10GQK6r4Q9lCruv_academia_logo_large.png',
+    academia: 'Academia X',
+    grado: 'Clase Maestra NODE JS',
+    fecha: '11 / Agosto / 2023',
+    subtitulo: 'Certificado',
+    link: 'https://docs.google.com/feeds/download/presentations/Export?id=1ixmljeNN16YadaXYeUZ2ji_inz5k_EjHPC0bbIwxJao&exportFormat=pdf',
+    nombreLink: 'Clase Maestra NODE JS',
+  },
+];
---
+++ src/mooks/projects.js
@@ -0,0 +1,71 @@
+const proyectos = [
+  //TaskAPP
+  {
+    title: 'TasksApp',
+    description:
+      'Crea tu cuenta, organiza tareas y accede desde cualquier dispositivo al instante.',
+    techs: 'React TS, Express JS, MySQL',
+    image:
+      'https://raw.githubusercontent.com/danuruiz7/tasksApp/main/imagen_preview/tasksApp-preview.png',
+    code: 'https://github.com/danuruiz7/tasksApp',
+    preview: 'https://tasks-app-by-danuruiz7.vercel.app/',
+  },
+  //ClonSpotify
+  {
+    title: 'ClonSpotify',
+    description:
+      'Crea tu cuenta, organiza tareas y accede desde cualquier dispositivo al instante.',
+    techs: 'Astro,ReactTS',
+    image:
+      'https://github.com/danuruiz7/portfolio/blob/main/src/img/spoti.png?raw=true',
+    code: 'https://github.com/danuruiz7/ClonSpotify',
+    preview: 'https://clon-spotify-two.vercel.app/',
+  },
+  //Quiz App
+  {
+    title: 'Quiz App',
+    description:
+      'App de trivias con distintas categorias para seleccionar ven a probarlas todas...!!!',
+    techs: 'React JS, React Router 6, Css',
+    image:
+      'https://github.com/danuruiz7/quizapp/blob/main/public/preview.png?raw=true',
+    code: 'https://github.com/danuruiz7/quizapp',
+    preview: 'https://quizapp-dr.vercel.app/',
+  },
+  //Lira Motors
+  {
+    title: 'Lira Motors',
+    description:
+      'Pagina web totalmente responsive creada con HTML, CSS, JavaScript para un taller mecanico real.',
+    techs: 'HTML, CSS, JavaScript',
+    image:
+      'https://github.com/danuruiz7/liramotorsweb/blob/main/img/preview.png?raw=true',
+
+    code: 'https://github.com/danuruiz7/liramotorsweb',
+
+    preview: 'https://liramotors.vercel.app/',
+  },
+  //Portfolio
+  {
+    title: 'Portfolio',
+    description:
+      'Portfolio personal para mostrar mis proyectos, experiencias, habilidades y muchas cosas mas...',
+    techs: 'Astro, Tailwind',
+    image: '/public/portfolio.png',
+    code: 'https://github.com/danuruiz7/portfolio/tree/main',
+    preview: '#',
+  },
+  //Weather App
+  {
+    title: 'Wheather App',
+    description:
+      'Proyecto realizado con React JS, utilizando el método fetch para llamar a la API de WeatherAPI y obtener datos meteorológicos en tiempo real.',
+    techs: 'React JS, Css, API',
+    image:
+      'https://raw.githubusercontent.com/danuruiz7/WeatherApp/main/public/preview-wahd.png',
+    code: 'https://github.com/danuruiz7/WeatherApp',
+    preview: 'https://weather-app-danudev.vercel.app',
+  },
+];
+
+export default proyectos;
