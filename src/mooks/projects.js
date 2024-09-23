ed7d1b777823d028e3fafba71bf0a4b6a00228fc
danuruiz7 <danuruiz7@hotmail.com>
2024-09-23 16:26:19 +0200

    actualizando los proyectos

--- src/mooks/projects.js
+++ src/mooks/projects.js
@@ -1,25 +1,14 @@
 const proyectos = [
-  //TaskAPP
+  //TattoCrm
   {
-    title: 'TasksApp',
+    title: 'TattoCrm',
     description:
-      'Crea tu cuenta, organiza tareas y accede desde cualquier dispositivo al instante.',
-    techs: 'React TS, Express JS, MySQL',
+      'App para gestion y registro de trabajos(tatuajes) que se hayan realizado los cliente y generacion automatica de pdf de consentimiento',
+    techs: 'Next TS, Tailwind CSS, Prisma ,PostgreSQL',
     image:
-      'https://raw.githubusercontent.com/danuruiz7/tasksApp/main/imagen_preview/tasksApp-preview.png',
-    code: 'https://github.com/danuruiz7/tasksApp',
-    preview: 'https://tasks-app-by-danuruiz7.vercel.app/',
-  },
-  //ClonSpotify
-  {
-    title: 'ClonSpotify',
-    description:
-      'Crea tu cuenta, organiza tareas y accede desde cualquier dispositivo al instante.',
-    techs: 'Astro,ReactTS',
-    image:
-      'https://github.com/danuruiz7/portfolio/blob/main/src/img/spoti.png?raw=true',
-    code: 'https://github.com/danuruiz7/ClonSpotify',
-    preview: 'https://clon-spotify-two.vercel.app/',
+      '/tattocrm.png',
+    code: 'https://github.com/danuruiz7/TattooCrm',
+    preview: 'https://tattocrm.druiz.dev/',
   },
   //Quiz App
   {
@@ -32,41 +21,74 @@ const proyectos = [
     code: 'https://github.com/danuruiz7/quizapp',
     preview: 'https://quizapp-dr.vercel.app/',
   },
-  //Lira Motors
+  //Pagina Web
   {
-    title: 'Lira Motors',
+    title: 'DC Pastel',
     description:
-      'Pagina web totalmente responsive creada con HTML, CSS, JavaScript para un taller mecanico real.',
-    techs: 'HTML, CSS, JavaScript',
+      'Pagina web para una tienda de tartas',
+    techs: 'Astro, Tailwind CSS',
     image:
-      'https://github.com/danuruiz7/liramotorsweb/blob/main/img/preview.png?raw=true',
+      '/dcpastel.png',
+    code: 'https://github.com/danuruiz7/quizapp',
+    preview: 'https://dcpastel.druiz.dev/',
+  },
+  // //TaskAPP
+  // {
+  //   title: 'TasksApp',
+  //   description:
+  //     'Crea tu cuenta, organiza tareas y accede desde cualquier dispositivo al instante.',
+  //   techs: 'React TS, Express JS, MySQL',
+  //   image:
+  //     'https://raw.githubusercontent.com/danuruiz7/tasksApp/main/imagen_preview/tasksApp-preview.png',
+  //   code: 'https://github.com/danuruiz7/tasksApp',
+  //   preview: 'https://tasks-app-by-danuruiz7.vercel.app/',
+  // },
+  // //ClonSpotify
+  // {
+  //   title: 'ClonSpotify',
+  //   description:
+  //     'Crea tu cuenta, organiza tareas y accede desde cualquier dispositivo al instante.',
+  //   techs: 'Astro,ReactTS',
+  //   image:
+  //     'https://github.com/danuruiz7/portfolio/blob/main/src/img/spoti.png?raw=true',
+  //   code: 'https://github.com/danuruiz7/ClonSpotify',
+  //   preview: 'https://clon-spotify-two.vercel.app/',
+  // },
+  // //Lira Motors
+  // {
+  //   title: 'Lira Motors',
+  //   description:
+  //     'Pagina web totalmente responsive creada con HTML, CSS, JavaScript para un taller mecanico real.',
+  //   techs: 'HTML, CSS, JavaScript',
+  //   image:
+  //     'https://github.com/danuruiz7/liramotorsweb/blob/main/img/preview.png?raw=true',
 
-    code: 'https://github.com/danuruiz7/liramotorsweb',
+  //   code: 'https://github.com/danuruiz7/liramotorsweb',
 
-    preview: 'https://liramotors.vercel.app/',
-  },
-  //Portfolio
-  {
-    title: 'Portfolio',
-    description:
-      'Portfolio personal para mostrar mis proyectos, experiencias, habilidades y muchas cosas mas...',
-    techs: 'Astro, Tailwind',
-    image:
-      'https://raw.githubusercontent.com/danuruiz7/portfolioAstro/main/public/portfolio.png',
-    code: 'https://github.com/danuruiz7/portfolioAstro/tree/main',
-    preview: '#',
-  },
-  //Weather App
-  {
-    title: 'Wheather App',
-    description:
-      'Proyecto realizado con React JS, utilizando el método fetch para llamar a la API de WeatherAPI y obtener datos meteorológicos en tiempo real.',
-    techs: 'React JS, Css, API',
-    image:
-      'https://raw.githubusercontent.com/danuruiz7/WeatherApp/main/public/preview-wahd.png',
-    code: 'https://github.com/danuruiz7/WeatherApp',
-    preview: 'https://weather-app-danudev.vercel.app',
-  },
+  //   preview: 'https://liramotors.vercel.app/',
+  // },
+  // //Portfolio
+  // {
+  //   title: 'Portfolio',
+  //   description:
+  //     'Portfolio personal para mostrar mis proyectos, experiencias, habilidades y muchas cosas mas...',
+  //   techs: 'Astro, Tailwind',
+  //   image:
+  //     'https://raw.githubusercontent.com/danuruiz7/portfolioAstro/main/public/portfolio.png',
+  //   code: 'https://github.com/danuruiz7/portfolioAstro/tree/main',
+  //   preview: '#',
+  // },
+  // //Weather App
+  // {
+  //   title: 'Wheather App',
+  //   description:
+  //     'Proyecto realizado con React JS, utilizando el método fetch para llamar a la API de WeatherAPI y obtener datos meteorológicos en tiempo real.',
+  //   techs: 'React JS, Css, API',
+  //   image:
+  //     'https://raw.githubusercontent.com/danuruiz7/WeatherApp/main/public/preview-wahd.png',
+  //   code: 'https://github.com/danuruiz7/WeatherApp',
+  //   preview: 'https://weather-app-danudev.vercel.app',
+  // },
 ];
 
 export default proyectos;
